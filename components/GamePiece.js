6c3ea161a40baba21dcc41b468bf0f0a91542a9b
Jon Wynveen <jonwynveen@gmail.com>
2018-11-17 21:57:22 +0200

    isConnected logic

--- components/GamePiece.js
+++ components/GamePiece.js
@@ -23,6 +23,12 @@ export default class GamePiece extends React.Component {
     this.scaleValue = new Animated.Value(0);
     this.elevationValue = new Animated.Value(0);
 
+    // Callback function to notify board (parent) of piece rotation state
+    this.onUpdate = props.onUpdate;
+    this.row = props.row;
+    this.column = props.column;
+    this.initialSides = props.initialSides.slice(0);
+
     this.state = {
       sides: props.initialSides || [0, 0, 0, 0],
       rotateState: 0,
@@ -72,7 +78,11 @@ export default class GamePiece extends React.Component {
     sides.unshift(lastSide);
     this.setState({ sides });
     console.log(this.state.sides);
-    // todo: event to send update to game board (or are props two-way)
+
+    // send update to game board
+    if (this.onUpdate) {
+      this.onUpdate(this.row, this.column, this.state.sides);
+    }
   }
 
   render() {
@@ -103,6 +113,10 @@ export default class GamePiece extends React.Component {
         onPress={this._onPress.bind(this)}
       >
         <Animated.View style={[styles.gamepiece, transformStyle]}>
+          <View style={[styles.sideMarker, styles.right, this.initialSides[0] === 0 ? styles.noConnection : null]}></View>
+          <View style={[styles.sideMarker, styles.bottom, this.initialSides[1] === 0 ? styles.noConnection : null]}></View>
+          <View style={[styles.sideMarker, styles.left, this.initialSides[2] === 0 ? styles.noConnection : null]}></View>
+          <View style={[styles.sideMarker, styles.top, this.initialSides[3] === 0 ? styles.noConnection : null]}></View>
           <Text style={styles.text}>{this.state.sides}</Text>
         </Animated.View>
       </TouchableWithoutFeedback>
@@ -121,5 +135,34 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#ffffff',
-  }
+  },
+  sideMarker: {
+    width: 10,
+    height: 10,
+    position: 'absolute',
+    backgroundColor: '#ffffff',
+  },
+  noConnection: {
+    backgroundColor: 'transparent',
+  },
+  top: {
+    top: 0,
+    left: 15,
+    height: 20,
+  },
+  bottom: {
+    bottom: 0,
+    left: 15,
+    height: 20,
+  },
+  right: {
+    right: 0,
+    top: 15,
+    width: 20,
+  },
+  left: {
+    left: 0,
+    top: 15,
+    width: 20,
+  },
 })
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -9,23 +9,129 @@ import {
 import GamePiece from '../components/GamePiece';
 
 export default class GameScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLevelComplete: false,
+      level: [
+        [
+          {sides: [1, 1, 0, 0], isConnected: false},
+          {sides: [0, 1, 1, 1], isConnected: false},
+          {sides: [1, 0, 0, 1], isConnected: false},
+        ],
+        [
+          {sides: [1, 1, 0, 1], isConnected: false},
+          {sides: [1, 1, 1, 1], isConnected: false},
+          {sides: [1, 1, 0, 1], isConnected: false},
+        ],
+        [
+          {sides: [1, 1, 0, 0], isConnected: false},
+          {sides: [0, 1, 1, 1], isConnected: false},
+          {sides: [1, 0, 0, 1], isConnected: false},
+        ],
+      ],
+    };
+  }
+  _onPieceUpdate(row, column, sides) {
+    // this.state.level[row][column].sides = sides;
+
+    const level = this.isConnected(this.state.level, row, column, sides, true);
+
+    const isLevelComplete = this.isLevelComplete(level);
+
+    this.setState({isLevelComplete, level});
+
+    // Check if level is all connected
+  }
+
+  isLevelComplete(level) {
+    let isLevelComplete = true;
+    for (let levelRow of level) {
+      for (let piece of levelRow) {
+        isLevelComplete = isLevelComplete && piece.isConnected;
+
+        if (!isLevelComplete) {
+          return false;
+        }
+      }
+    }
+    return isLevelComplete;
+  }
+  isConnected(level, row, column, sides, checkChild) {
+
+    const lastRow = level[row - 1];
+    const currentRow = level[row];
+    const nextRow = level[row + 1];
+
+    let isConnected = true;
+
+    // isSideConnect (1)
+    if (currentRow && currentRow[column + 1]) {
+      const neighborPiece = currentRow[column + 1];
+      const isSideConnected = sides[0] === neighborPiece.sides[2]
+      isConnected = isConnected && isSideConnected;
+      if (checkChild) {
+        this.isConnected(level, row, column + 1, neighborPiece.sides);
+      }
+    }
+    // isSideConnect (2)
+    if (nextRow && nextRow[column]) {
+      const neighborPiece = nextRow[column];
+      const isSideConnected = sides[1] === neighborPiece.sides[3];
+      isConnected = isConnected && isSideConnected;
+      if (checkChild) {
+        this.isConnected(level, row + 1, column, neighborPiece.sides);
+      }
+    }
+    // isSideConnect (3)
+    if (currentRow && currentRow[column - 1]) {
+      const neighborPiece = currentRow[column - 1];
+      const isSideConnected = sides[2] === neighborPiece.sides[0];
+      isConnected = isConnected && isSideConnected;
+      if (checkChild) {
+        this.isConnected(level, row, column - 1, neighborPiece.sides);
+      }
+    }
+    // isSideConnect (4)
+    if (lastRow && lastRow[column]) {
+      const neighborPiece = lastRow[column];
+      const isSideConnected = sides[3] === neighborPiece.sides[1];
+      isConnected = isConnected && isSideConnected;
+      if (checkChild) {
+        this.isConnected(level, row - 1, column, neighborPiece.sides);
+      }
+    }
+
+    level[row][column].isConnected = isConnected;
+
+    return level;
+  }
+
   render() {
+
+    const rows = this.state.level.map((row, rowIndex) => {
+      return (
+        <View style={styles.row} key={'row' + (rowIndex + 1)}>
+          {row.map((piece, columnIndex) =>
+            <GamePiece
+              initialSides={piece.sides}
+              isConnected={piece.isConnected}
+              onUpdate={this._onPieceUpdate.bind(this)}
+              row={rowIndex}
+              column={columnIndex}
+              key={(rowIndex + 1) + '.' + (columnIndex + 1)}
+            />
+          )}
+        </View>
+      )
+    });
     return (
       <View style={styles.gameboard}>
-        <View style={styles.row}>
-          <GamePiece initialSides={[1, 1, 0, 0]} />
-          <GamePiece initialSides={[0, 1, 0, 1]} />
-          <GamePiece initialSides={[1, 0, 0, 1]} />
-        </View>
-        <View style={styles.row}>
-          <GamePiece initialSides={[1, 1, 0, 1]} />
-          <GamePiece initialSides={[1, 1, 1, 1]} />
-          <GamePiece initialSides={[1, 1, 0, 1]} />
-        </View>
-        <View style={styles.row}>
-          <GamePiece initialSides={[1, 1, 0, 0]} />
-          <GamePiece initialSides={[0, 1, 0, 1]} />
-          <GamePiece initialSides={[1, 0, 0, 1]} />
+        {rows}
+        <View style={styles.debug}>
+          <Text>{JSON.stringify(this.state.level, null, true)}</Text>
+          <Text>Level Complete: {this.state.isLevelComplete ? 'true' : 'false'}</Text>
         </View>
       </View>
     );
